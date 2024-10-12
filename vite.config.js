c17332d9740380fa314936c858caad754c347835
dimalsha <madushaniagd@gmail.com>
2024-10-12 17:08:00 +0530

    update backend url

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
   
   return {
   server: {
     proxy: {
       '/api': {
-        target: `http://${env.VITE_HOST}:3000`,
+        target: 'https://trainease-backend.onrender.com',
         secure: false,
       },
     },
